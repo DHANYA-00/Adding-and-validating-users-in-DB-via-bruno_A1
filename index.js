3a878592865c2939c697fce855abcb5581629a62
Dhanya Lakshmi s s <dhanyalakshmi.s.s.06@gmail.com>
2025-03-20 10:53:30 +0530

    Completed

--- index.js
+++ index.js
@@ -1,8 +1,55 @@
 const express = require('express');
 const { resolve } = require('path');
+const mongoose=require('mongoose');
+const User=require('./schema');
+const { error } = require('console');
+const bcrypt=require('bcrypt');
+require('dotenv').config();
 
 const app = express();
 const port = 3010;
+const URI=process.env.URI;
+app.use(express.json());
+
+
+mongoose.connect(URI)
+.then(()=>{
+  console.log("MongoDB Connected Successfully")
+})
+.catch((error)=>{
+  console.log("Failed to connect",error)
+})
+
+app.post('/create',async(req,res)=>{
+  try{
+    const{UserName,Email,Password}=req.body;
+
+    if(!UserName||!Email||!Password){
+      return res.status(400).json({
+        error:"All fields are required"
+      })
+    }
+
+    const saltRound=12;
+    const hashedPassword = await bcrypt.hash(Password,saltRound);
+
+    const newUser = new User({
+      UserName:UserName,Email:Email,Password:hashedPassword
+    })
+    await newUser.save();
+    return res.status(200).json({
+      message:"Created Successfully"
+    });
+  }
+  catch{
+    return res.status(500).json({
+        message:"Internal Server Error"
+    });
+  }
+})
+
+  
+
 
 app.use(express.static('static'));
 
